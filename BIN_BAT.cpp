5b21e2c452d7a170f9cfbca3445b2abe52c06aee
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-10-21 00:13:52 +0530

    Add files via upload

---
+++ BIN_BAT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;  
+    cin >> T;
+    
+    while (T--) {
+        int N, A, B;
+        cin >> N >> A >> B;
+        int rounds = log2(N);  
+        int total = (rounds * A) + ((rounds - 1) * B);
+        cout << total << endl;
+    }
+    return 0;
+}
