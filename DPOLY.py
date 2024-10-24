589bce1e8b68ba125f38a368614829553eb810a6
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-10-24 22:39:03 +0530

    Add files via upload

---
+++ DPOLY.py
@@ -0,0 +1,12 @@
+def polynomial(test_cases):
+    for t in range(test_cases):
+        N = int(input()) 
+        coefficients = list(map(int, input().split()))   
+        for i in range(N-1, -1, -1):
+            if coefficients[i] != 0:
+                print(i)
+                break
+
+T = int(input())
+
+polynomial(T)
