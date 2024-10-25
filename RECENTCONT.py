61321f22cc932c488a8dacc052fa4f800869b96c
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-10-25 16:19:23 +0530

    Add files via upload

---
+++ RECENTCONT.py
@@ -0,0 +1,7 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    contest = input().split()
+    start38 = contest.count("START38")
+    ltime108 = contest.count("LTIME108")
+    print(start38, ltime108)
