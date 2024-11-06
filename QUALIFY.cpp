fe20dc8101b21060da8c4b077ce198d5033c2177
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-11-06 22:26:18 +0530

    Add files via upload

---
+++ QUALIFY.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, A, B;
+        cin >> X >> A >> B;
+
+        int total = A + 2 * B;
+
+        if (total >= X) {
+            cout << "Qualify" << endl;
+        } else {
+            cout << "NotQualify" << endl;
+        }
+    }
+}
