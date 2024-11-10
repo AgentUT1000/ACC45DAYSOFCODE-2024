a45826ff6d1637b60a8c101ee0326fb0b0809ff5
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-11-10 14:19:30 +0530

    Add files via upload

---
+++ CYCLICQD.py
@@ -0,0 +1,9 @@
+T = int(input().strip())
+
+for _ in range(T):
+    A, B, C, D = map(int, input().strip().split())
+    
+    if A + C == 180 and B + D == 180:
+        print("YES")
+    else:
+        print("NO")
