48d477878b57af4a222f8da85c35b9e64c0d9731
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2024-11-13 23:22:49 +0530

    Add files via upload

---
+++ SALE.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        
+        int total = A + B + C;
+        int disco = min({A, B, C});
+        int final = total - disco;
+        
+        cout << final<< endl;
+    }
+}
